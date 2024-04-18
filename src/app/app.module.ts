fae021769da5f5d6c743c89d0f40096b77b6a631
Thaodien2203 <dienthao131@gmail.com>
2024-04-18 22:32:18 +0700

    push project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/common/header/header.component';
+import { IonicModule } from '@ionic/angular';
+import {NgOptimizedImage} from "@angular/common";
+import { BannerComponent } from './component/common/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        IonicModule.forRoot({}),
+        NgOptimizedImage,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
